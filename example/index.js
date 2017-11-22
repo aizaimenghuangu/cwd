33fb00592f05b6248d6dc5192f98cc86f9338e7a
熊金飞 <xiongjinfei@star-net.cn>
2017-11-22 16:23:30 +0800

    1、实现coding webhook 钉钉

--- example/index.js
+++ example/index.js
@@ -3,14 +3,32 @@
 const express = require('express'),
     bodyParser = require('body-parser'),
     CWD = require('./../index'),
-    app = express();
+    app = express(),
+morgan = require('morgan');
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(morgan('dev'));
 
-app.post('/webhook', CWD());
+if ('development' == app.get('env')) {
+    // app.use(errorHandler());
+    console.log('开发者模式');
+    app.use(function(err, req, res, next) {
+        log.info(JSON.stringify(err));
+        res.render('error', {
+            message: err.message,
+            title: err.message,
+            result: err.toString(),
+            error: err
+        });
+    });
+}
 
+console.log(app.get('env'));
+
+
+app.post('/', CWD());
 app.listen(5566, function () {
-    console.log('Express server listening on port http://localhost:6666');
+    console.log('Express server listening on port http://localhost:5566');
 });
--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 
-
-
 const request = require('request');
 
 exports = module.exports = function (options) {
@@ -9,6 +7,8 @@ exports = module.exports = function (options) {
 
         let event = req.headers['x-coding-event'],
             body = req.body;
+
+        console.log(event);
 
         switch (event){
             case "push":
@@ -28,25 +28,28 @@ exports = module.exports = function (options) {
 function Push(req, res, next) {
 
     let body = req.body;
+    console.log(body);
     let ding = body.token;
     let count = body.commits.length;
+    let desc = body.repository.description;
     let whopush = body.commits[0].committer.name;
     let repourl = `${body.repository.owner.web_url}/p/${body.repository.name}/git/tree/${getbranch(body.ref)}`;
     let branch = `# [[${body.repository.name}/${getbranch(body.ref)}]](${repourl})`;
-    let text = `${branch} ${count} commit,push by ${whopush}\n ## commits:\n`;
+    let text = `${desc}\n`;
+    text += `${branch} ${count} commit,push by ${whopush}\n ## 更新内容:\n`;
     for (let i = 0; i < count; i++){
         let com = body.commits[i];
         let name = com.committer.name;
         let mes = com.short_message;
         let hash6 = com.sha.substring(0,6);
-        let commit = `> </> ${name}: [[${hash6}]](${com.web_url})-${mes}\n`;
+        let commit = `> ${name}: [[${hash6}]](${com.web_url})-${mes}\n`;
         text = text + commit;
     }
-    console.log(text);
+    console.log("text==", text);
     let rqbody = {
         msgtype: "markdown",
         markdown: {
-            title:"仓库有新的代码更新咯!",
+            title: "【更新】"+desc,
             text: text
         }
     };
